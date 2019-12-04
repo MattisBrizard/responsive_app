0a98ce826c80abeefb4db7480b9dbbd524e03b0a
Mattis Brizard <matttis.brizard@orange.com>
2019-12-04 13:45:57 +0100

    updated main colors

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   AppColors._();
   static const Color mainColor = Color(0XFFED5C56);
-  // static const Color red = Color(0XFFED5C56);
+  // static const Color grey = Color(0XFF383838);
 }
